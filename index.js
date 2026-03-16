71540352ced343b993351c9b32ffa4c681586934
Teewara <teewara.pud@student.mahidol.edu>
2026-03-16 15:32:33 +0700

    Fix typo in response message from "Hell" to "Hello"

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const requestListener = (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hell World from DevOps Lab!\n');
+    res.end('Hello World from DevOps Lab!\n');
 };
 
 const app = http.createServer(requestListener);
